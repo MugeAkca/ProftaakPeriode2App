f1ecc6ccb0feb6c91e6353f2ad8725b1d32472c2
MugeAkca <48129976+MugeAkca@users.noreply.github.com>
2020-01-12 19:47:59 +0100

    unit test

--- app/src/androidTest/java/com/example/runningapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/runningapp/ExampleInstrumentedTest.java
@@ -23,6 +23,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.runningapp", appContext.getPackageName());
+        assertEquals("com.RunningApp.runningapp", appContext.getPackageName());
     }
 }
