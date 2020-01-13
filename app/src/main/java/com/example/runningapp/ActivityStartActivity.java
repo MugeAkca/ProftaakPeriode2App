4807c7bd6fc6e94a233c4eba77f81ebbfe6082ef
Scramper <33483324+Scramper@users.noreply.github.com>
2020-01-13 21:29:20 +0100

    Step counter added

--- app/src/main/java/com/example/runningapp/ActivityStartActivity.java
+++ app/src/main/java/com/example/runningapp/ActivityStartActivity.java
@@ -13,6 +13,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.CursorIndexOutOfBoundsException;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -24,6 +28,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -53,9 +58,12 @@ import java.util.Timer;
 import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 
-public class ActivityStartActivity extends FragmentActivity implements OnMapReadyCallback {
+public class ActivityStartActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
     //Clock
     private Chronometer timer;
+
+
+
 
     //Map
     private MapView mapView;
@@ -73,6 +81,13 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
     private PolylineOptions routeOps;
     private List<LatLng> routePoints;
 
+    //Step Sensor
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private boolean isSensorPresent = false;
+    private TextView mStepsSinceReboot;
+    private int stepsSinceReboot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,6 +103,25 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
                 v.getContext().startActivity(intent);
             }
         });
+        //First check permissions
+        if(!checkPermissions()) {
+            requestPermissions();
+        }
+
+
+        //Step sensor
+        mStepsSinceReboot = findViewById(R.id.step);
+
+        mSensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null)
+        {
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isSensorPresent = true;
+        }
+        else{
+            isSensorPresent = false;
+        }
+
 
 
         //Clock
@@ -110,9 +144,6 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
         simpleChronometer.start(); // start a chronometer
 
         //Map
-        if(!checkPermissions()) {
-            requestPermissions();
-        }
         fusedLocationProviderClient = getFusedLocationProviderClient(this);
         getLastLocation();
         startLocationUpdates();
@@ -126,7 +157,10 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
     @Override
     protected void onResume() {
         super.onResume();
-        //startLocationUpdates();
+        startLocationUpdates();
+        if(isSensorPresent){
+            mSensorManager.registerListener(this, mSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     @Override
@@ -142,11 +176,15 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
     protected void onPause() {
         super.onPause();
         stopLocationUpdates();
+        if(isSensorPresent){
+            mSensorManager.unregisterListener(this);
+        }
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+
+        stopLocationUpdates();
     }
     @Override
     public void onLowMemory() {
@@ -155,7 +193,6 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         if(googleMap == null){
             return;
         }
@@ -171,7 +208,6 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
             }
         };
 
-
         //Draw a line from where the user starts
         routeOps = new PolylineOptions()
                 .color(Color.BLUE)
@@ -185,10 +221,25 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
             gmap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
         }
     }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int calculatedSteps = Math.round(event.values[0]);
+        if(stepsSinceReboot  < 1){
+            stepsSinceReboot = Math.round(event.values[0]);
 
+        }
+
+        mStepsSinceReboot.setText(Integer.toString(calculatedSteps - stepsSinceReboot));
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 
     // Trigger new location updates at interval
-    protected void startLocationUpdates() {
+    public void startLocationUpdates() {
 
         // Create the location request to start receiving updates
         locationRequest = new LocationRequest();
@@ -217,7 +268,10 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
     }
 
     public void stopLocationUpdates(){
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        if(locationCallback != null)
+        {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        }
     }
 
     public void onLocationChanged(Location location) {
@@ -266,7 +320,8 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
     //Permission check methods
     private boolean checkPermissions() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
         return false;
@@ -275,9 +330,14 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
     private void requestPermissions() {
         ActivityCompat.requestPermissions(
                 this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACTIVITY_RECOGNITION
+                },
                 PERMISSION_ID
         );
+
     }
 
     private boolean isLocationEnabled() {
@@ -296,5 +356,6 @@ public class ActivityStartActivity extends FragmentActivity implements OnMapRead
             }
         }
     }
+
 
 }
